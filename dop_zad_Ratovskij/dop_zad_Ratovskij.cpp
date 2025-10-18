6c59203f2d3ee342776d29682fd7198e107dba15
mightykk7 <ratovskijmaks@gmail.com>
2025-10-18 14:56:54 +0500

    Add files via upload

---
+++ dop_zad_Ratovskij/dop_zad_Ratovskij.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+struct EventSystem {
+    void (*handlers[100])();
+    int count = 0;
+};
+void registerHandler(EventSystem& system, void(*handler)()) {
+    system.handlers[system.count] = handler;
+    system.count++;
+}
+void triggerEvent(const EventSystem& system) {
+    for (int i = 0; i < 4; i++) {
+        system.handlers[i]();
+    }
+}
+void handler(const std::string& message) {
+    std::cout << message << std::endl;
+}
+void onUserLogin() {
+    std::cout << "Пользователь вошёл в систему" << std::endl;
+}
+void onUserLogout() {
+    std::cout << "Пользователь вышел из системы" << std::endl;
+}
+void onError() {
+    std::cout << "Произошла ошибка!" << std::endl;
+}
+int main()
+{
+    setlocale(0, "rus");
+    EventSystem system;
+    registerHandler(system, &onUserLogin);
+    registerHandler(system, &onUserLogout);
+    registerHandler(system, &onError);
+    triggerEvent(system);
+}
