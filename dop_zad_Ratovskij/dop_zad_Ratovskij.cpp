95511b596e8669e70ab16ba73070321724c4a362
mightykk7 <ratovskijmaks@gmail.com>
2025-10-25 12:34:22 +0500

    Add files via upload

--- dop_zad_Ratovskij/dop_zad_Ratovskij.cpp
+++ dop_zad_Ratovskij/dop_zad_Ratovskij.cpp
@@ -1,36 +1,40 @@
 ﻿
 #include <iostream>
 struct EventSystem {
-    void (*handlers[100])();
+    void (*handlers[100])(const std::string&);
     int count = 0;
 };
-void registerHandler(EventSystem& system, void(*handler)()) {
-    system.handlers[system.count] = handler;
-    system.count++;
-}
-void triggerEvent(const EventSystem& system) {
-    for (int i = 0; i < 4; i++) {
-        system.handlers[i]();
+
+void registerHandler(EventSystem& system, void(*handler)(const std::string&)) {
+    if (system.count < 100) {
+        system.handlers[system.count] = handler;
+        system.count++;
     }
 }
-void handler(const std::string& message) {
-    std::cout << message << std::endl;
+
+void triggerEvent(const EventSystem& system, const std::string& message) {
+    for (int i = 0; i < system.count; ++i) {
+        system.handlers[i](message);
+    }
 }
-void onUserLogin() {
-    std::cout << "Пользователь вошёл в систему" << std::endl;
+
+void onUserLogin(const std::string& message) {
+    std::cout << "Пользователь вошёл в систему: " << message << std::endl;
 }
-void onUserLogout() {
-    std::cout << "Пользователь вышел из системы" << std::endl;
+
+void onUserLogout(const std::string& message) {
+    std::cout << "Пользователь вышел из системы: " << message << std::endl;
 }
-void onError() {
-    std::cout << "Произошла ошибка!" << std::endl;
+
+void onError(const std::string& message) {
+    std::cout << "Произошла ошибка: " << message << std::endl;
 }
 int main()
 {
     setlocale(0, "rus");
     EventSystem system;
-    registerHandler(system, &onUserLogin);
-    registerHandler(system, &onUserLogout);
-    registerHandler(system, &onError);
-    triggerEvent(system);
+    registerHandler(system, onUserLogin);
+    registerHandler(system, onUserLogout);
+    registerHandler(system, onError);
+    triggerEvent(system, "Тестовое сообщение");
 }
